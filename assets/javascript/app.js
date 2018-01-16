e9c9163e3f862e86df818a8876be1c5a9a5d068e
Julie Groth <groth.julie@gmail.com>
2018-01-15 20:46:29 -0800

    added bootstrap, jQuery, and Firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBSCTKAZ5ImjnHZTw-iVOy3qLX4KEjFLRI",
+    authDomain: "train-time-44a0d.firebaseapp.com",
+    databaseURL: "https://train-time-44a0d.firebaseio.com",
+    projectId: "train-time-44a0d",
+    storageBucket: "",
+    messagingSenderId: "434063813925"
+  };
+  firebase.initializeApp(config);
